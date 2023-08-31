1838070afd43a12e48303a6e7059c8d5a9f26995
Hekimianz <aramhekdesign@gmail.com>
2023-08-31 15:10:28 -0500

    Implemented Main Menu Design

---
+++ index.js
@@ -0,0 +1,12 @@
+// const getChamp = async function (name) {
+//   const res = await fetch(
+//     `http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion/${name}.json`
+//   );
+//   const { data } = await res.json();
+//   console.log(data[`${name}`]);
+//   img.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${name}_0.jpg`;
+// };
+
+// getChamp("Bard");
+
+// const img = document.querySelector("img");
